1a47467c5e27f82f1972298ed8c6d9a50287a8c0
Klaus Alfert <klaus.alfert@googlemail.com>
2015-06-04 15:46:34 +0200

    first tests

--- test/eurexa_test.exs
+++ test/eurexa_test.exs
@@ -1,7 +1,26 @@
 defmodule EurexaTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Eurexa.EurexaServer
+
+  test "make a full URL" do
+  	app = "myapp"
+  	hostname = "eurexa.catcal.zone"
+  	eureka_host = "eureka.catcal.zone"
+  	base_url = "http://#{eureka_host}:8761/eureka/v2/apps"
+
+  	assert "http://#{eureka_host}:8761/eureka/v2/apps/#{app}/#{hostname}" == 
+  		EurexaServer.make_url(base_url, app, hostname)
   end
+
+  test "make a post URL" do
+  	app = "myapp"
+  	# hostname = "eurexa.catcal.zone"
+  	eureka_host = "eureka.catcal.zone"
+  	base_url = "http://#{eureka_host}:8761/eureka/v2/apps"
+
+  	assert "http://#{eureka_host}:8761/eureka/v2/apps/#{app}" == 
+  		EurexaServer.make_url(base_url, app)
+  end
+
 end
