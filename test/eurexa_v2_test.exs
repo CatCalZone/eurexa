70f7456665eefebb76807f2ba55fa5cb40747212
Klaus Alfert <klaus.alfert@googlemail.com>
2015-06-07 18:53:45 +0200

    match bodies and adapt for EurekaV2

--- test/eurexa_v2_test.exs
+++ test/eurexa_v2_test.exs
@@ -2,25 +2,35 @@ defmodule EurexaTest do
   use ExUnit.Case
 
   alias Eurexa.EurexaServer
+  alias Eurexa.EurekaV2
 
   test "make a full URL" do
   	app = "myapp"
   	hostname = "eurexa.catcal.zone"
   	eureka_host = "eureka.catcal.zone"
-  	base_url = "http://#{eureka_host}:8761/eureka/v2/apps"
+  	base_url = "http://#{eureka_host}:8761/"# "eureka/v2/apps"
 
   	assert "http://#{eureka_host}:8761/eureka/v2/apps/#{app}/#{hostname}" == 
-  		EurexaServer.make_url(base_url, app, hostname)
+  		EurekaV2.make_url(base_url, app, hostname)
   end
 
   test "make a post URL" do
   	app = "myapp"
   	# hostname = "eurexa.catcal.zone"
   	eureka_host = "eureka.catcal.zone"
-  	base_url = "http://#{eureka_host}:8761/eureka/v2/apps"
+  	base_url = "http://#{eureka_host}:8761/"
 
   	assert "http://#{eureka_host}:8761/eureka/v2/apps/#{app}" == 
-  		EurexaServer.make_url(base_url, app)
+  		EurekaV2.make_url(base_url, app)
+  end
+
+  test "json data for registering" do
+  	app = %EurexaServer{}
+  	json = EurexaServer.make_instance_data(app)
+
+  	assert ~r{"app" *: *"my_app"} |> Regex.match? json
+    assert ~r{"hostName" *: *"localhost"} |> Regex.match? json
+    assert ~r{"ipAddr" *: *"127.0.0.1"} |> Regex.match? json
   end
 
 end
