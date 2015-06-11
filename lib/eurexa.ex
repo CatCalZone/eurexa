4a9a297b17e5ecdef03bac23f9c938726a8a52c1
Klaus Alfert <klaus.alfert@googlemail.com>
2015-06-11 19:23:03 +0200

    Workers generates the list of Supervisor Worker definitions depending on the configuration

--- lib/eurexa.ex
+++ lib/eurexa.ex
@@ -16,4 +16,23 @@ defmodule Eurexa do
     opts = [strategy: :one_for_one, name: Eurexa.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  @doc """
+  Creates the Eurexa worker definitions for various apps given as parameter. 
+  It is checked that a configuration for those apps exists and only for 
+  existing configurations the worker specs are created. 
+
+  This helpful for testing and dev environemnts where potenitally the 
+  Eurexa configuraiton is not available yet starting the application 
+  should be done alway in the same way 
+  """
+  @spec workers(atom | [atom]) :: [term]
+  def workers(app) when is_atom(app), do: workers([app]) 
+  def workers(apps) when is_list(apps) do
+    import Supervisor.Spec, warn: false
+    apps 
+      |> Enum.filter(fn app -> nil != Application.get_env(:eurexa, app) end)
+      |> Enum.map(fn app -> worker(Eurexa.EurexaServer, [app]) end)
+  end
+  
 end
